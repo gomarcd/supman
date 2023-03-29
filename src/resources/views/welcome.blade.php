50b4c10bb14c64fce93c3b2ef46dd0a977db4b6c
gomarcd <git@gomarcd.dev>
2023-03-29 12:20:04 -0600

    add favicon

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Supman</title>
-
-         @vite(['resources/css/flatpickr.min.css', 'resources/css/app.css', 'resources/js/app.js'])
-         @livewireStyles
+            <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" sizes="32x32">
+            @vite(['resources/css/flatpickr.min.css', 'resources/css/app.css', 'resources/js/app.js'])
+            @livewireStyles
     </head>
 
     <body>
